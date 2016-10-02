1b09f4a86d5d17e6e441f38023a37cf6632e1294
hbhslevel3history <2jcoxhea@hbhs.school.nz>
2016-10-02 19:06:45 +1300

    Update find5.js

--- find5.js
+++ find5.js
@@ -642,7 +642,6 @@ function create_div(dleft, dtop, dwidth, dheight)
 	+ ';cursor: default' // make mouse arrow stay an arrow instead of turning to text arrow
 	+ ';font-weight: bold'
 	+ ';background-color: red'
-	+ ';color: white'
 	+ ';float: right'
 	+ ';text-align: center'
 	+ ';width: 20px'
